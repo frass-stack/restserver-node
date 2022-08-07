0781e8883041e3c647d929d8e32fb2bf4d7100e3
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-06 23:02:36 -0300

    Creamos el model usuario. Configuramos el mismo con los campos que tendra la collection usuario.

---
+++ models/usuario.js
@@ -0,0 +1,46 @@
+//Representacion del modelo:
+/*{
+    nombre:'user',
+    correo: 'user@user.com',
+    password:'12345',
+    img:'1234566767',
+    rol:'1234556678',
+    estado:false, // true:activo, false:eliminado
+    google:false //true:creado mediante google, false:creado mediante app
+}*/
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required:[true, 'El nombre es requerido']
+    },
+    correo: {
+        type: String,
+        required:[true, 'El correo es requerido'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required:[true, 'El password es requerido']
+    },
+    img: {
+        type: String,
+    },
+    rol: {
+        type: String,
+        required:true,
+        enum:['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default:true
+    },
+    google: {
+        type: Boolean,
+        default:false
+    }
+});
+
+module.exports = model('Usuario', UsuarioSchema);
