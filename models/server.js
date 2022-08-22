c177ea5a9a87d56a8b4797e059222f10199cc78b
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-22 17:41:10 -0300

    Crear ruta autentificacion - Auth - login.

--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.userPath = '/api/users';
+        this.authPath = '/api/auth';
 
         //Conectar a la DB
         this.conectarDB();
@@ -35,6 +36,7 @@ class Server {
     }
 
     routes(){
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.userPath, require('../routes/user'));
     }
 
