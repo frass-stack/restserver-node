41f1838d1d57754bc0c38bc31bdba63f5217c51f
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-26 21:32:14 -0300

    Login de Usuario.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,49 @@
 const { response } = require("express");
+const bcriptjs = require('bcryptjs');
+
+const Usuario = require("../models/usuario");
 
 
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
 
+    const { correo, password } = req.body;
 
-    res.json({
-        msg: 'Login OK'
-    })
+    try {
 
+        const usuario = await Usuario.findOne( {correo} );
+
+        //Validar la existencia de usuario
+        if(!usuario){
+            return res.status(400).json({
+                msg:'Usuario / Password incorrectos o no existen en el Sistema. - correo'
+            })
+        }
+
+        //Validar el estado del usuario
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg:'Usuario / Password incorrectos o no existen en el Sistema. - estado:false'
+            })
+        }
+
+        //validar el password del usuario
+        const validPassword = bcriptjs.compareSync( password, usuario.password )
+
+        if( !validPassword ){
+            return res.status(400).json({
+                msg:'Usuario / Password incorrectos o no existen en el Sistema. - password'
+            })
+        }
+
+        res.json({
+            msg: 'Login OK'
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Hable con el administrador.'
+        })
+    }
 }
 
 module.exports = {
